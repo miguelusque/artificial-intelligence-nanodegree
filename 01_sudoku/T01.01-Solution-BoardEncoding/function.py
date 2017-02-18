e6fd2fe7bb3213c9e70fac2b3e032df4f17a7662
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-02-18 20:41:37 +0100

    Initial commit

---
+++ 01_sudoku/T01.01-Solution-BoardEncoding/function.py
@@ -0,0 +1,18 @@
+from utils import *
+
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+    """
+    
+    result = {}
+    for i in range(len(boxes)):
+        result[boxes[i]] = grid[i]
+   
+    return result
