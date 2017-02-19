f53ea59af5779fcede1ed72da93967a37c589de2
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-02-19 04:48:07 +0100

    Initial commit

---
+++ 01_sudoku/T01.04-Exercise-OnlyChoiceStrategy/function.py
@@ -0,0 +1,13 @@
+from utils import *
+
+def only_choice(values):
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    # TODO: Implement only choice strategy here
+    return values
