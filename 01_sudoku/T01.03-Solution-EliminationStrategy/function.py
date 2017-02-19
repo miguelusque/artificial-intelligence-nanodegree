cec283cc2a51ba3e93f71eb3e85b5334397633b2
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-02-19 04:41:02 +0100

    Initial commit

---
+++ 01_sudoku/T01.03-Solution-EliminationStrategy/function.py
@@ -0,0 +1,26 @@
+from utils import *
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    
+    GRID_LENGTH = 81
+    EMPTY_VALUE = '.'
+    ANY_VALUE = '123456789'
+    
+    assert len(values) == GRID_LENGTH, "Input grid must be a string of length 81 (9x9)"
+    
+    for key, value in values.items():
+        if len(value) == 1:
+            for peer in peers[key]:
+                values[peer] = values[peer].replace(value, '')
+
+    return values
